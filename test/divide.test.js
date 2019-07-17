df19e601837a7ba9bfc35e83ebe6c1640758d796
Andrew <andrewyin1994@gmail.com>
2019-07-17 08:40:26 -0700

    mocha drill complete

---
+++ test/divide.test.js
@@ -0,0 +1,15 @@
+const { expect } = require('chai');
+
+const divide = require('../index');
+
+describe('divide function', () => {
+  
+  it('should divide positive integers correctly', () => {
+    expect(divide(10, 5)).to.equal(2);
+  });
+
+  it('should throw an error when divided by 0', () => {
+    expect(() => divide(10, 0)).to.throw();
+  });
+  
+});
